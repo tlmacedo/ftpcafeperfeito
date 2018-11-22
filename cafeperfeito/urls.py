2fcf9bafba24ba213c5e8872914596a328af6e03
Thiago Macedo <tl.macedo@hotmail.com>
2018-11-21 20:50:47 -0400

    auto-commit

--- cafeperfeito/urls.py
+++ cafeperfeito/urls.py
@@ -1,10 +1,8 @@
-from django.conf import settings
 from django.conf.urls import url
-from django.conf.urls.static import static
 
 from cafeperfeito import views
 
 urlpatterns = [
-                  url(r'^$', views.index, name='index'),
-                  # url(r'^login/$', views.login, name='login'),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    url(r'^$', views.index, name='index'),
+    # url(r'^login/$', views.login, name='login'),
+]
