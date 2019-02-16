4509814b0e5848ba068d95b746c2b71955786aab
Thiago Macedo <tl.macedo@hotmail.com>
2019-02-16 11:32:47 -0400

    auto-commit

--- cafeperfeito/urls.py
+++ cafeperfeito/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from cafeperfeito.views import LoginTemplateView, HomeTemplateView, ProdutosListView, ProdutoCreateView, \
@@ -6,10 +8,13 @@ from cafeperfeito.views import LoginTemplateView, HomeTemplateView, ProdutosList
 app_name = 'cafeperfeito'
 
 urlpatterns = [
-    path('', LoginTemplateView.as_view(), name='login'),
-    path('home/', HomeTemplateView.as_view(), name='home'),
-    path('produtos/', ProdutosListView.as_view(), name='lista_produtos'),
-    path('produto/cadastrar/', ProdutoCreateView.as_view(), name='produto_cadastrar'),
-    path('produto/<pk>', ProdutoUpdateView.as_view(), name='produto_atualizar'),
+                  path('', LoginTemplateView.as_view(), name='login'),
+                  path('home/', HomeTemplateView.as_view(), name='home'),
+                  path('produtos/', ProdutosListView.as_view(), name='lista_produtos'),
+                  path('produto/cadastrar/', ProdutoCreateView.as_view(), name='produto_cadastrar'),
+                  path('produto/<pk>', ProdutoUpdateView.as_view(), name='produto_atualizar'),
 
-]
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ftpcafeperfeito/settings.py
+++ ftpcafeperfeito/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -75,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -158,5 +161,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = '/home/cafeperfeito/www/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = '/home/cafeperfeito/www/static'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
