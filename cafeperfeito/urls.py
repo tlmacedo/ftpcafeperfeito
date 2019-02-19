b8cb3b6c1cf2ca405bc9dbfaaa83e9749d9012bb
Thiago Macedo <tl.macedo@hotmail.com>
2019-02-19 14:50:49 -0400

    auto-commit

--- cafeperfeito/urls.py
+++ cafeperfeito/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
 from cafeperfeito.views import LoginTemplateView, HomeTemplateView, ProdutosListView, ProdutoCreateView, \
@@ -8,12 +6,12 @@ from cafeperfeito.views import LoginTemplateView, HomeTemplateView, ProdutosList
 app_name = 'cafeperfeito'
 
 urlpatterns = [
-                  path('', LoginTemplateView.as_view(), name='login'),
-                  path('home/', HomeTemplateView.as_view(), name='home'),
-                  path('produtos/', ProdutosListView.as_view(), name='lista_produtos'),
-                  path('produto/cadastrar/', ProdutoCreateView.as_view(), name='produto_cadastrar'),
-                  path('produto/<pk>', ProdutoUpdateView.as_view(), name='produto_atualizar'),
-]  ## + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', LoginTemplateView.as_view(), name='login'),
+    path('home/', HomeTemplateView.as_view(), name='home'),
+    path('produtos/', ProdutosListView.as_view(), name='lista_produtos'),
+    path('produto/cadastrar/', ProdutoCreateView.as_view(), name='produto_cadastrar'),
+    path('produto/<pk>', ProdutoUpdateView.as_view(), name='produto_atualizar'),
+]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
