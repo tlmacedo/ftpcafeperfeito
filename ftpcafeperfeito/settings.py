262b7f2517c03d6718f9c290d50d81b0e0dc06dd
thiagomacedo <tl.macedo@hotmail.com>
2020-04-10 13:00:31 -0400

    auto commit

--- ftpcafeperfeito/settings.py
+++ ftpcafeperfeito/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#-hh^%3o^m83b4m0ue$951u%0&e(x%gx5ib1(k(v+eogwv12tg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'www.cafeperfeito.com.br']
 
