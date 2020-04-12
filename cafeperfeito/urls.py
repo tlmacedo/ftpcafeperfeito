7fd506b14e5a5eb2d8d1ef3a0483a442ebfb8fa4
thiagomacedo <tl.macedo@hotmail.com>
2020-04-11 23:36:45 -0400

    auto commit

--- cafeperfeito/urls.py
+++ cafeperfeito/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('', LoginTemplateView.as_view(), name='login'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
