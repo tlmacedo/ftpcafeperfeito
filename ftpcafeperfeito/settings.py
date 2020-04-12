9fc5c3d2dcfa90af947b4c1d1de7da2581ffef3e
thiagomacedo <tl.macedo@hotmail.com>
2020-04-11 22:56:07 -0400

    auto commit

--- ftpcafeperfeito/settings.py
+++ ftpcafeperfeito/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 ]
 
