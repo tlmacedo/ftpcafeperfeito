2805b985c1cdf1479d79b34606adee7725d3061a
thiagomacedo <tl.macedo@hotmail.com>
2020-04-12 20:42:16 -0400

    auto commit

--- ftpcafeperfeito/settings.py
+++ ftpcafeperfeito/settings.py
@@ -150,8 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = '/home/cafeperfeito/www/static'
 
-# MEDIA_URL = 'media/'
-# MEDIA_ROOT = '/home/cafeperfeito/www/media'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = '/home/cafeperfeito/www/media'
